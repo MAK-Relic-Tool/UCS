98a9b739c67963834a1f839263879edf81a16de9
Marcus Kertesz <ModernMAK@Hotmail.com>
2025-03-10 01:20:16 -0700

    Modernize Repo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,16 +13,17 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Relic Tool - UCS'
-copyright = '2022, Marcus Kertesz'
-author = 'Marcus Kertesz'
+project = "Relic Tool - UCS"
+copyright = "2022, Marcus Kertesz"
+author = "Marcus Kertesz"
 
 # The full version, including alpha/beta/rc tags
 from relic.ucs import __version__ as package_version
+
 release = package_version
 
 # -- General configuration ---------------------------------------------------
@@ -32,13 +33,13 @@ release = package_version
 # ones.
 
 extensions = [
-    'sphinx.ext.autodoc',  # Core library for html generation from docstrings
-    'sphinx.ext.autosummary',  # Create neat summary tables
+    "sphinx.ext.autodoc",  # Core library for html generation from docstrings
+    "sphinx.ext.autosummary",  # Create neat summary tables
 ]
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -50,9 +51,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
